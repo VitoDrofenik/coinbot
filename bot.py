23ab9e49da7bdf2402d4fc776817ba89855a43dd
Vito <vitodro@gmail.com>
2020-11-23 23:25:00 +0100

    added info command

--- bot.py
+++ bot.py
@@ -2,6 +2,15 @@ import yfinance as yf
 import discord
 from datetime import datetime
 from discord.ext import commands
+import math
+
+# this is from SO
+millnames = ['', ' Thousand', ' Million', ' Billion', ' Trillion']
+def millify(n):
+    n = float(n)
+    millidx = max(0, min(len(millnames)-1,
+                        int(math.floor(0 if n == 0 else math.log10(abs(n))/3))))
+    return '{:.0f}{}'.format(n / 10 ** (3 * millidx), millnames[millidx])
 
 
 ### ZAGON BOTA
@@ -22,18 +31,20 @@ async def on_ready():
 async def price(ctx, *, query):
     try:
         ticker = yf.Ticker(query)
+        currency = " " + ticker.info["currency"]
         embed = discord.Embed(
             title=ticker.info["shortName"],
             color=discord.Color.green()
         )
         embed.set_thumbnail(url=ticker.info["logo_url"])
-        embed.add_field(name="Current price", value=str(ticker.info["ask"])+"$", inline=False)
+        embed.add_field(name="Current price", value="{:.2f}".format(ticker.info["ask"]) + currency, inline=False)
     except:
         embed = discord.Embed(
             title="Ticker not found",
             color=discord.Color.red()
         )
         embed.add_field(name="Invalid ticker.", value=" Please try again.", inline=False)
+        print("[ error ] "+query+" not found")
     await ctx.send(embed=embed)
 
 
@@ -47,16 +58,48 @@ async def indepth(ctx, *, query):
             color=discord.Color.green()
         )
         embed.set_thumbnail(url=ticker.info["logo_url"])
-        embed.add_field(name="Current price", value=str(ticker.info["ask"]) + currency, inline=False)
-        embed.add_field(name="Open", value=str(ticker.info["open"]) + currency, inline=True)
-        embed.add_field(name="High", value=str(ticker.info["dayHigh"]) + currency, inline=True)
-        embed.add_field(name="Low", value=str(ticker.info["dayLow"]) + currency, inline=True)
+        embed.add_field(name="Current price", value="{:.2f}".format(ticker.info["ask"]) + currency, inline=False)
+        embed.add_field(name="Open", value="{:.2f}".format(ticker.info["open"]) + currency, inline=True)
+        embed.add_field(name="High", value="{:.2f}".format(ticker.info["dayHigh"]) + currency, inline=True)
+        embed.add_field(name="Low", value="{:.2f}".format(ticker.info["dayLow"]) + currency, inline=True)
+        embed.add_field(name="Previous close", value="{:.2f}".format(ticker.info["previousClose"]) + currency, inline=False)
+        embed.add_field(name="Market cap", value=millify(ticker.info["marketCap"]), inline=False)
     except:
         embed = discord.Embed(
             title="Ticker not found",
             color=discord.Color.red()
         )
         embed.add_field(name="Invalid ticker.", value=" Please try again.", inline=False)
+        print("[ error ] "+query+" not found")
+    await ctx.send(embed=embed)
+
+
+@client.command(name="info")
+async def info(ctx, *, query):
+    try:
+        ticker = yf.Ticker(query)
+        currency = " " + ticker.info["currency"]
+        embed = discord.Embed(
+            title=ticker.info["shortName"],
+            color=discord.Color.green()
+        )
+        embed.set_thumbnail(url=ticker.info["logo_url"])
+        embed.add_field(name="Long name", value=ticker.info["longName"], inline=False)
+        embed.add_field(name="Industry", value=ticker.info["industry"], inline=False)
+        embed.add_field(name="Address", value=ticker.info["address1"], inline=False)
+        embed.add_field(name="City", value=ticker.info["city"], inline=True)
+        embed.add_field(name="State", value=ticker.info["state"], inline=True)
+        embed.add_field(name="Country", value=ticker.info["country"], inline=True)
+        embed.add_field(name="Website", value=ticker.info["website"], inline=False)
+        embed.add_field(name="Phone", value=ticker.info["phone"], inline=True)
+        #embed.add_field(name="Summary", value=ticker.info["longBusinessSummary"], inline=False)
+    except:
+        embed = discord.Embed(
+            title="Ticker not found",
+            color=discord.Color.red()
+        )
+        embed.add_field(name="Invalid ticker.", value=" Please try again.", inline=False)
+        print("[ error ] "+query+" not found")
     await ctx.send(embed=embed)
 
 
